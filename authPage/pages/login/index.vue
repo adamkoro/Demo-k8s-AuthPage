a8dd817d10e5e0c3bdb4b2ba8dbabfba4ed173b3
adamkoro <adam.koronics@gmail.com>
2023-02-05 22:06:21 +0100

    feat: theme switcher works with one button

--- authPage/pages/login/index.vue
+++ authPage/pages/login/index.vue
@@ -4,9 +4,8 @@
             <h1 class="text-4xl text-black text-center dark:text-white">
                 Login Page
             </h1>
-            <img src="~/assets/login.png" width="90"/>
+            <nuxt-img src="/login.png" loading="lazy" width="100"/>
         </div>
-        <!-- Create a login form-->
         <form class="flex flex-col items-center py-10 gap-1 text-black dark:text-white" @submit.prevent="login">
             <label for="email">Email</label>
             <input type="email" id="email" v-model="email" placeholder="Enter email" required/>
@@ -14,17 +13,18 @@
             <input type="password" id="password" v-model="password" placeholder="Enter password" required />
             <button type="submit" class="bg-orange-500 text-white border-black border-2 py-2 px-5">Login</button>
         </form>
-        <!-- Create theme switcher button -->
         <div class="flex flex-row items-center justify-center gap-2 py-8">
-            <button class="bg-orange-500 text-white border-black border-2 py-2 px-5" @click="setColorTheme('light')">Light</button>
-            <button class="bg-orange-500 text-white border-black border-2 py-2 px-5" @click="setColorTheme('dark')">Dark</button>
+            <button class="bg-orange-500 text-white border-black border-2 py-2 px-5" @click="setColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')">
+                <Icon name="material-symbols:dark-mode" v-if="$colorMode.value == 'dark'" />
+                <Icon name="material-symbols:light-mode" v-else />
+            </button>
         </div>
     </div>
 </template>
 
 <script setup>
 
-let Theme = 'light' | 'dark';
+//let Theme = 'light' | 'dark';
 const setColorTheme = (Theme) => {
     useColorMode().preference = Theme;
 }
